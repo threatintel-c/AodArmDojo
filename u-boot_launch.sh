6fdfe03947b8d7b3b266dd03c2e389cb3b63b2fb
Atul Alex Cherian <mr.atulalex@gmail.com>
2018-05-10 12:28:17 +0530

    Update u-boot_launch.sh

--- u-boot_launch.sh
+++ u-boot_launch.sh
@@ -1,2 +1,2 @@
-qemu-system-arm -M vexpress-a9 -dtb dtb/vexpress-v2p-ca9.dtb -m 512M -serial stdio -append "root=/dev/mmcblk0 console=ttyAMA0" -kernel u-boot
+qemu-system-arm -M vexpress-a9 -dtb dtb/vexpress-v2p-ca9.dtb -m 512M -serial stdio -append "root=/dev/mmcblk0 rw physmap.enabled=0 console=ttyAMA0" -kernel u-boot
 
